34a686cfb28070ba5f1fb32660e9a3e189549a44
David <149296106+dmeleroandrade@users.noreply.github.com>
2024-07-19 07:37:12 +0000

    data model instagram hecho de david

--- src/models.py
+++ src/models.py
@@ -1,97 +1,86 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, LargeBinary
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+ 
 
-class User(Base):
-    __tablename__ = 'user'
+class Usuario(Base):
+    __tablename__ = 'usuario'
+   
+    id_usuario = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    nombre_usuario = Column(String(250))
+    email = Column(String(250), nullable=False)
+    contraseña =  Column(String(250),nullable=False)
+    fecha_registro=(Column(Date))
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(20), nullable = False)
-    firstname = Column(String(50), nullable = False)
-    lastname = Column(String(50), nullable = False)
-    email = Column(String(50), nullable = False)
-    password = Column(String(50), nullable = False)
+    def to_dict(self):
+        return {}
 
-    posts = relationship('Post', back_populates = 'user')
-    comments = relationship('Comment', back_populates = 'user')
-    followers = relationship('Follower', foreign_keys='Follower.user_to_id', back_populates='user_to')
-    following = relationship('Follower', foreign_keys='Follower.user_from_id', back_populates='user_from')
+class Publicacion(Base):
+    __tablename__ = 'publicacion'
+   
+    id_publicacion = Column(Integer, primary_key=True)
+    image = Column(LargeBinary, nullable = True)
+    descripcion =  Column(String(250))
+    fecha_publicacion=(Column(Date))
+    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'))
+    usuario = relationship('Usuario')
 
-class Follower(Base):
-    __tablename__ = 'follower'
+    def to_dict(self):
+        return {}
 
-    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
-    user_from = relationship('User', foreign_keys=[user_from_id], back_populates='following')
-    user_to = relationship('User', foreign_keys=[user_to_id], back_populates='followers')
+class Comentario(Base):
+    __tablename__ = 'comentario'
+   
+    id_comentario = Column(Integer, primary_key=True)
+    comentario =  Column(String(250))
+    fecha_comentario=(Column(Date))
+    id_publicacion = Column(Integer, ForeignKey('publicacion.id_publicacion'))
+    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'))
+    usuario = relationship('Usuario')
+    publicacion = relationship('Publicacion')
 
-class Characters(Base):
-    __tablename__ = 'characters'
 
-    id= Column(Integer, primary_key=True)
-    name= Column(String(250), nullable=False)
-    gender=Column(String(250))
-    height=Column(String(250))
-    hair_color=Column(String(250))
-    eye_color=Column(String(250))
-    birth_year=Column(Integer)
+    def to_dict(self):
+        return {}
 
-class Planets(Base):
-    __tablename__= 'planets'
+    
+class Like(Base):
+    __tablename__ = 'like'
+   
+    id_like = Column(Integer, primary_key=True)
+    id_publicacion = Column(Integer, ForeignKey('publicacion.id_publicacion'))
+    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'))
+    fecha_like=(Column(Date))
+    usuario = relationship('Usuario')
+    publicacion = relationship('Publicacion')
 
-    id=Column(Integer, primary_key=True)
-    name= Column(String(250), nullable=False)
-    climate= Column(String(250))
-    population= Column(Integer)
-    diameter= Column(Float)
-    terrain = Column(String)
-    surface_water= Column(Integer)
-    orbital_period= Column(Integer)
+    def to_dict(self):
+        return {}
+    
 
-class Vehicles(Base):
-    __tablename__= 'vehicles'
+class Seguir(Base):
+    __tablename__ = 'Seguir'
+   
+    id_Seguir = Column(Integer, primary_key=True)
+    id_usuario_seguido = Column(Integer, ForeignKey('usuario.id_usuario'))
+    id_usuario_seguidor = Column(Integer, ForeignKey('usuario.id_usuario'))
+    fecha_seguimiento =(Column(Date))
+    usuario = relationship('Usuario')
+    
 
-    id= Column(Integer, primary_key=True)
-    name= Column(String(250), nullable=False)
-    model_name= Column(String(250))
-    manufacturer= Column(String(250))
-    price= Column(Integer)
-
-class Favorites(Base):
-    __tablename__= 'favorites'
-
-    id= Column(Integer, primary_key=True) 
-    user_id= Column(Integer, ForeignKey('user.id'))
-    characters_id= Column(Integer, ForeignKey('characters.id'))
-    planets_id= Column(Integer, ForeignKey('planets.id'))
-    vehicles_id= Column(Integer, ForeignKey('vehicles.id'))
-
-class Post(Base):
-    __tablename__= 'post'
-
-    id= Column(Integer, primary_key=True) 
-    description= Column(Text)
-    body= Column(Text)
-    user_id= Column(Integer, ForeignKey('user.id'))
-
-    user= relationship('User', back_populates='posts')
-    comment= relationship('Comment', back_populates='post')
-
-class Comment(Base):
-    __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(Text, nullable=False)
-    author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-
-    user = relationship('User', back_populates='comments')
-    post = relationship('Post', back_populates='comments')
-
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+    def to_dict(self):
+        return {}
+    
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
